080218a0d1d6fd683fb1ccfb654ffc6a4676c747
IamHerica <hericacadoni@gmail.com>
2020-10-11 21:23:57 -0300

    Create 1004.c

---
+++ C/1004.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int v1, v2, PROD;
+    scanf("%int", &v1);
+    scanf("\n%int", &v2);
+    PROD = (v1 * v2);
+    printf("PROD = %d\n", PROD);
+ 
+    return 0;
+}
