bf498dac458c8fdb9f5fe39e67fe5f83761d042f
Herica Cadoni <71198776+IamHerica@users.noreply.github.com>
2020-10-20 23:05:10 -0300

    Add files via upload

---
+++ C/1007.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   int A, B, C, D,Diferenca;
+   
+   scanf("%d", &A);
+   scanf("%d", &B);
+   scanf("%d", &C);
+   scanf("%d", &D);
+   
+   Diferenca = ((A*B)- (C*D));
+   
+   printf("DIFERENCA = %d\n", Diferenca);
+ 
+    return 0;
+}
