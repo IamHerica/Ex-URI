a356fbd40737d1c31cb2a2511b6b83aaad2164c0
Herica Cadoni <71198776+IamHerica@users.noreply.github.com>
2020-11-04 18:47:14 -0300

    Update 1021.c

--- C/1021.c
+++ C/1021.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
- 
+
 int main() {
- 
-   int A, B, C, D;
-   
-   scanf("%d %d %d %d", &A, &B, &C, &D);
-   
-   if ((B > C) && (D > A) && ((C +D)> (A +B)) && (C && D) > 0 && (A % 2 == 0))
-   printf("Valores aceitos\n");
-   else
-   printf("Valores nao aceitos\n");
- 
-    return 0;
+  double n, moedas[6] = {1, .5, .25, .1, .05, .01};
+  int notas, moeda, cedulas[6] = {100, 50, 20, 10, 5, 2};
+  
+  scanf("%lf", &n);
+  
+  printf("NOTAS:\n");
+
+  int contador = 0;
+
+  while (contador < 6) {
+
+    notas = (int) n / cedulas[contador];
+    n -= notas * cedulas[contador];
+
+    printf("%i nota(s) de R$ %i.00\n", notas, cedulas[contador++]);
+
+  }
+
+  printf("MOEDAS:\n");
+
+  contador = 0;
+  
+  while (contador < 5) {
+
+    moeda = (int) (n / moedas[contador]);
+    n -= (double) moeda * moedas[contador];
+
+    printf("%i moeda(s) de R$ %.2lf\n", moeda, moedas[contador++]);
+
+  }
+  printf("%.lf moeda(s) de R$ 0.01\n", n * 100);
+
+  return 0;
 }
